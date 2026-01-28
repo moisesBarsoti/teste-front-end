db2409389c3c570d533c4ccb28bf80cab338642b
Moisés Barsoti <moises.barsot2@gmail.com>
2026-01-28 09:49:42 -0300

    feat(Header): criado a sectionLogo
    Adicionado a logo Econverse, criado o input de pesquisa e as opções de usuário

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,17 @@
 import './index.css';
-import ShieldCheck from "../../assets/icons/ShieldCheck.svg"
-import Truck from "../../assets/icons/Truck.svg"
-import CreditCard from "../../assets/icons/CreditCard.svg"
+import ShieldCheck from "../../assets/icons/ShieldCheck.svg";
+import Truck from "../../assets/icons/Truck.svg";
+import CreditCard from "../../assets/icons/CreditCard.svg";
+import Logo from "../../assets/icons/Logo.svg"; 
+import MagnifyingGlass from "../../assets/icons/MagnifyingGlass.svg";
+import Caixa from "../../assets/icons/Caixa.svg";
+import Heart from "../../assets/icons/Heart.svg";
+import UserCircle from "../../assets/icons/UserCircle.svg";
+import ShoppingCart from "../../assets/icons/ShoppingCart.svg";
 
 export default function Header() {
     return (
-        <header>
+        <header aria-label="Cabeçalho da página">
             <ul>
                 <li>
                     <img src={ShieldCheck} alt="Icone ShieldCheck" />
@@ -20,7 +26,44 @@ export default function Header() {
                     <p><span>Parcele</span> suas compras</p>
                 </li>
             </ul>
-            <div></div>
+            <div className="sectionLogo">
+                <span>
+                    <img src={Logo} alt="Logo Econverse" />
+                </span>
+                <div className="pesquisa-wrapper">
+                    <input
+                        type="text"
+                        placeholder="O que você está buscando?"
+                        className="pesquisa-input"
+                        aria-label="Campo de pesquisa"
+                    />
+                    <button className="pesquisa-button" type="submit" aria-label="Botão de pesquisa" title="Pesquisar">
+                        <img src={MagnifyingGlass} alt="Icone MagnifyingGlass" />
+                    </button>
+                </div>
+                <ul>
+                    <li>
+                        <button title="Meus Pedidos">
+                            <img src={Caixa} alt="Icone Caixa" />
+                        </button>
+                    </li>
+                    <li>
+                        <button title="Lista de Desejos">
+                            <img src={Heart} alt="Icone Heart" />
+                        </button>
+                    </li>
+                    <li>
+                        <button title="Minha Conta">
+                            <img src={UserCircle} alt="Icone UserCircle" />
+                        </button>
+                    </li>
+                    <li>
+                        <button title="Carrinho de Compras">
+                            <img src={ShoppingCart} alt="Icone ShoppingCart" />
+                        </button>
+                    </li>
+                </ul>
+            </div>
             <nav></nav>
         </header>
     )
