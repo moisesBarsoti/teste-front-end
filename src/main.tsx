53ae148ae63545bcf77aeb60068bdf0d60b39ccb
Moisés Barsoti <moises.barsot2@gmail.com>
2026-01-29 09:49:51 -0300

    feat(main): criado a rota OfertasDoDia e todasCategorias

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './globals.css'
+import './globals.scss'
 import App from './App.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import TodasCategorias from './pages/TodasCategorias/index.tsx'
+import OfertasDoDia from './pages/OfertasDoDia/index.tsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     errorElement: <div>Erro ao carregar a página</div>,
+    children: [
+      {path: "/", element: <OfertasDoDia />},
+      {path: "/todasCategorias", element: <TodasCategorias />},
+    ]
   },
 ]);
 
