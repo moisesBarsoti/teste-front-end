42c0b399fbaf88f5c5fe4e3b4af2493d573a7f70
Moisés Barsoti <moises.barsot2@gmail.com>
2026-01-30 10:40:52 -0300

    feat(VitrineCategoria): realizado o map para cada categoria

--- src/components/VitrineCategoria/index.tsx
+++ src/components/VitrineCategoria/index.tsx
@@ -1,9 +1,22 @@
 import './index.scss';
+import CardVitrine from '../CardVitrine';
+import { vitrineCategoria } from '../../data/vitrineCategoria';
+import { useState } from 'react';
 
 export default function VitrineCategoria() {
+    const [categoriaAtiva, setCategoriaAtiva] = useState<number | null>(0);
     return (
         <div className="vitrine-categoria">
-            
+            {vitrineCategoria.map((categoria, index) => (
+                <CardVitrine 
+                    key={index}
+                    src={categoria.src}
+                    alt={categoria.alt}
+                    span={categoria.span}
+                    isAtivo={categoriaAtiva === index}
+                    onToggle={() => setCategoriaAtiva(index)}
+                />
+            ))}
         </div>
     )
 }
