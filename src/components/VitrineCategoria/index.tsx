31bef5c0bf1c1f3184a36d3e2dfe8987d9050094
Moisés Barsoti <moises.barsot2@gmail.com>
2026-01-30 11:03:34 -0300

    feat(VitrineCategoria): realizado animação de aparecer
    Cada card vai apresentar um por vez até que chegue no ultimo card

--- src/components/VitrineCategoria/index.tsx
+++ src/components/VitrineCategoria/index.tsx
@@ -4,18 +4,24 @@ import { vitrineCategoria } from '../../data/vitrineCategoria';
 import { useState } from 'react';
 
 export default function VitrineCategoria() {
-    const [categoriaAtiva, setCategoriaAtiva] = useState<number | null>(0);
+    const [categoriaAtiva, setCategoriaAtiva] = useState(0);
+
     return (
         <div className="vitrine-categoria">
             {vitrineCategoria.map((categoria, index) => (
-                <CardVitrine 
+                <div 
                     key={index}
-                    src={categoria.src}
-                    alt={categoria.alt}
-                    span={categoria.span}
-                    isAtivo={categoriaAtiva === index}
-                    onToggle={() => setCategoriaAtiva(index)}
-                />
+                    className="card-vitrine-item"
+                    style={{ '--index': index } as React.CSSProperties}
+                >
+                    <CardVitrine 
+                        src={categoria.src}
+                        alt={categoria.alt}
+                        span={categoria.span}
+                        isAtivo={categoriaAtiva === index}
+                        onToggle={() => setCategoriaAtiva(index)}
+                    />
+                </div>
             ))}
         </div>
     )
