5b34a7bdedbd0f0ed78099674dd71397f8f771e6
Moisés Barsoti <moises.barsot2@gmail.com>
2026-01-30 10:42:30 -0300

    fea(OfertasDoDia): importado VitrineCategoria

--- src/pages/OfertasDoDia/index.tsx
+++ src/pages/OfertasDoDia/index.tsx
@@ -1,9 +1,11 @@
 import BannerBlackFriday from "../../components/BannerBlackFriday";
+import VitrineCategoria from "../../components/VitrineCategoria";
 
 export default function OfertasDoDia() {
     return (
         <section>
             <BannerBlackFriday />
+            <VitrineCategoria />
         </section>
     )
 }
