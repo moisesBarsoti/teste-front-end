3310bccede895ea79ab88bde99b6c057431ca52d
Moisés Barsoti <moises.barsot2@gmail.com>
2026-01-31 19:50:44 -0300

    feat(OfertasDoDia): importado componente CardParceiro

--- src/pages/OfertasDoDia/index.tsx
+++ src/pages/OfertasDoDia/index.tsx
@@ -1,13 +1,19 @@
+import "./index.scss";
 import BannerBlackFriday from "../../components/BannerBlackFriday";
+import CardParceiro from "../../components/CardParceiro";
 import ProdutosRelacionados from "../../components/ProdutosRelacionados";
 import VitrineCategoria from "../../components/VitrineCategoria";
 
 export default function OfertasDoDia() {
     return (
-        <section>
+        <section className="ofertas-do-dia">
             <BannerBlackFriday />
             <VitrineCategoria />
             <ProdutosRelacionados />
+            <div className="container-parceiros">
+                <CardParceiro />
+                <CardParceiro />
+            </div>
         </section>
     )
 }
