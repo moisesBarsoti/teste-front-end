7964d810c93f8738314cdfb9d07827b3b296e553
Moisés Barsoti <moises.barsot2@gmail.com>
2026-02-01 00:05:41 -0300

    feat(Footer): criado componente Footer
    Feito a importação do componente newsletter

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+import Newsletter from "../Newsletter";
+import "./index.scss";
+
+export default function Footer() {
+    return (
+        <footer>
+            <Newsletter />
+        </footer>
+    )
+}
