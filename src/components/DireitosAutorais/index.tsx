7d57ed6a168f0ef925f44a01c9d107eb6bc4625d
Moisés Barsoti <moises.barsot2@gmail.com>
2026-02-01 10:50:20 -0300

    feat(DireitosAutorais): criado componente DireitosAutorais

---
+++ src/components/DireitosAutorais/index.tsx
@@ -0,0 +1,9 @@
+import './index.scss';
+
+export default function DireitosAutorais() {
+    return (
+        <div className="direitos-autorais">
+            <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h6>
+        </div>
+    )
+}
