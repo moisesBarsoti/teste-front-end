f7117495f06167ca0e5dffca02f60d92e8b41459
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-26 21:41:37 -0300

    chore: feito instalação do sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-function App() {
+import { Outlet } from 'react-router-dom'
 
+function App() {
   return (
-    <>
-    </>
+    <div className="app-layout">
+      <main className="app-content">
+        <Outlet />
+      </main>
+    </div>
   )
 }
 
