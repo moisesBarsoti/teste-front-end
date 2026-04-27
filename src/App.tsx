e30e223cf07abc2d9df8f2319621bd2af4fec569
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-26 22:43:39 -0300

    feat(App): importado componente Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import './App.scss';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="app-layout">
+      <Header />
       <main className="app-content">
         <Outlet />
       </main>
