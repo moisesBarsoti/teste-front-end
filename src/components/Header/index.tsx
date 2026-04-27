2199f58d24966d11f0d8ccf130b37d54b1c1851d
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-26 22:43:05 -0300

    feat(Header): feito criação do Header
    E adicionado o TopBar

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import TopBar from '../TopBar';
+import './index.scss';
+
+export default function Header() {
+    return (
+        <header aria-label="Cabeçalho da página">
+            <TopBar />
+        </header>
+    )
+}
