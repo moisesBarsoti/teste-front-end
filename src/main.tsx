fc6b8f54d872ff4b325ad8be1a36f1ef802e148e
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-27 08:55:02 -0300

    chore: limpeza nos arquivos

--- src/main.tsx
+++ src/main.tsx
@@ -21,5 +21,5 @@ const router = createBrowserRouter([
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
+  </StrictMode>
 )
