f39d4843c01d903704ecd4668670def149cb9057
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-26 23:42:19 -0300

    feat(Header): importado MainHeader e criado hr

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,13 @@
 import TopBar from '../TopBar';
+import MainHeader from '../MainHeader';
 import './index.scss';
 
 export default function Header() {
     return (
         <header aria-label="Cabeçalho da página">
             <TopBar />
+            <div className='line'></div>
+            <MainHeader />
         </header>
     )
 }
