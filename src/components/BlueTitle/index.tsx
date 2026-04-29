40144282383ab2fe40752f570e0077738ea8051a
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-28 23:14:45 -0300

    feat(BlueTitle): criado componente BlueTitle

---
+++ src/components/BlueTitle/index.tsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import type { BlueTitleType } from "../../types/BlueTitleType";
+import "./style.css";
+
+export default function BlueTitle({link, title, line = true}: BlueTitleType) {
+  return (
+    <div className="container-title-blue">
+      <div className="title-blue">
+        {line && <div className="line"></div>}
+        <h3>{title}</h3>
+        {line && <div className="line"></div>}
+      </div>
+      {link && (
+        <div className="container-link">
+          <Link
+            to="/produtos"
+            className="see-all-links"
+            title="Ver todos os produtos"
+          >
+            Ver todos
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+}
