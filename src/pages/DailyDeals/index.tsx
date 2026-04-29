c1741f0ae0e7bfbe75f99f2156813b9828239a2f
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-29 01:23:34 -0300

    feat(DailyDeals): feito importação RelatedProducts

--- src/pages/DailyDeals/index.tsx
+++ src/pages/DailyDeals/index.tsx
@@ -20,6 +20,8 @@ export default function DailyDeals() {
             <ContainerPartner />
 
             <ContainerMark />
+            
+            <RelatedProducts tags seeAll/>
         </section>
     )
 }
